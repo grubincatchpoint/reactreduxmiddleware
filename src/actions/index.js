b2628c3e171020608a905da5ba8483332f990ab6
Greg Rubin <grubin@catchpoint.com>
2017-05-17 16:42:17 -0400

    imported axiom and updated the request search

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '9675ca63e51dc5a5602fe8e65c8951de';
 const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-export default class SearchBar extends Component {
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
 onInputChange(event) {
@@ -16,6 +21,8 @@ onFormSubmit(event) {
   event.preventDefault();
 
   //need to go fetch weather data
+  this.props.fetchWeather(this.state.term);
+  this.setState({ term: '' });
 }
 
   render() {
@@ -33,3 +40,9 @@ onFormSubmit(event) {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
