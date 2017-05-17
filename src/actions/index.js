828aa0aafd3b91604ba7935216dec1f4e7447d82
Greg Rubin <grubin@catchpoint.com>
2017-05-17 14:28:59 -0400

    added api url and installed axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 const API_KEY = '9675ca63e51dc5a5602fe8e65c8951de';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
